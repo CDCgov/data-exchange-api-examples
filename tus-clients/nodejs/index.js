c21c37a43efed4454c18777f20528493187542ce
cfarmer <cfarmer@fearless.tech>
2023-02-13 10:14:40 -0500

    script code

---
+++ tus-clients/nodejs/index.js
@@ -0,0 +1,37 @@
+
+'use strict'
+
+require('dotenv').config();
+const fs = require('fs');
+const tus = require('tus-js-client');
+
+const path = `${__dirname}/10MB-test-file`;
+const file = fs.createReadStream(path);
+
+const options = {
+  endpoint: `${process.env.DEX_URL}/upload`,
+  headers : {
+    Authorization: `Bearer ${process.env.AUTH_TOKEN}`,
+  },
+  metadata: {
+    filename           : '10MB-test-file',
+    filetype           : 'text/plain',
+    meta_destination_id: 'ndlp',
+    meta_ext_event     : 'ri',
+  },
+  onError (error) {
+    console.error('An error occurred:')
+    console.error(error)
+    process.exitCode = 1
+  },
+  onProgress (bytesUploaded, bytesTotal) {
+    const percentage = ((bytesUploaded / bytesTotal) * 100).toFixed(2)
+    console.log(bytesUploaded, bytesTotal, `${percentage}%`)
+  },
+  onSuccess () {
+    console.log('Upload finished:', upload.url)
+  },
+}
+
+const upload = new tus.Upload(file, options)
+upload.start()
