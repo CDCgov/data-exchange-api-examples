334c60cd87286f2dce767afc4fc29a57913f410d
cfarmer <cfarmer@fearless.tech>
2023-02-16 15:55:14 -0500

    required fields and uuid package

--- tus-clients/nodejs/index.js
+++ tus-clients/nodejs/index.js
@@ -4,6 +4,7 @@
 require('dotenv').config({ path: '../../.env' });
 const fs = require('fs');
 const tus = require('tus-js-client');
+const { v4: uuidv4 } = require('uuid');
 
 const path = `${__dirname}/../../upload-files/10MB-test-file`;
 const file = fs.createReadStream(path);
@@ -14,10 +15,16 @@ const options = {
     Authorization: `Bearer ${process.env.AUTH_TOKEN}`,
   },
   metadata: {
-    filename           : '10MB-test-file',
-    filetype           : 'text/plain',
-    meta_destination_id: 'ndlp',
-    meta_ext_event     : 'ri',
+    filename              : '10MB-test-file',
+    filetype              : 'text/plain',
+    meta_destination_id   : 'ndlp',
+    meta_ext_event        : 'ri',
+    meta_ext_source       : 'IZGW',
+    meta_ext_sourceversion: 'V2022-12-31',
+    meta_ext_entity       : 'DD2',
+    meta_username         : 'ygj6@cdc.gov',
+    meta_ext_objectkey    : uuidv4(),
+    meta_ext_filename     : '10MB-test-file'
   },
   onError (error) {
     console.error('An error occurred:')
