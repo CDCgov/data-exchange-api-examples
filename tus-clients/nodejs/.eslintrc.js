b6612a0cfa2b573cdd1c40f1fd516c7f87be3b42
cfarmer <cfarmer@fearless.tech>
2023-02-16 16:02:14 -0500

    prettier

--- tus-clients/nodejs/.eslintrc.js
+++ tus-clients/nodejs/.eslintrc.js
@@ -1,12 +1,12 @@
 module.exports = {
-  "env": {
-      "browser": true,
-      "node": true,
-      "es2021": true
+  env: {
+    browser: true,
+    node: true,
+    es2021: true,
   },
-  "extends": "eslint:recommended",
-  "parserOptions": {
-      "ecmaVersion": "latest",
-      "sourceType": "module"
+  extends: "eslint:recommended",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
-}
+};
