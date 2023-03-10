4aa751088149d1b618c64314e3971844d87a040b
cfarmer <cfarmer@fearless.tech>
2023-03-10 14:19:53 -0500

    code for java upload

--- tus-clients/java/src/main/java/Main.java
+++ tus-clients/java/src/main/java/Main.java
@@ -1,2 +1,68 @@
+import io.github.cdimascio.dotenv.Dotenv;
+import io.tus.java.client.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.UUID;
+
 public class Main {
+    public static void main(String[] args) throws IOException, ProtocolException {
+        try {
+            Dotenv dotenv = Dotenv.configure()
+                    .directory("../../.env")
+                    .load();
+
+            HashMap<String, String> headerMap = new HashMap<>();
+            headerMap.put("Authorization", "Bearer " + dotenv.get("AUTH_TOKEN"));
+
+            HashMap<String, String> metadataMap = new HashMap<>();
+            metadataMap.put("filename", "10MB-test-file");
+            metadataMap.put("filetype", "text/plain");
+            metadataMap.put("meta_destination_id", "ndlp");
+            metadataMap.put("meta_ext_event", "ri");
+            metadataMap.put("meta_ext_source", "IZGW");
+            metadataMap.put("meta_ext_sourceversion", "V2022-12-31");
+            metadataMap.put("meta_ext_entity", "DD2");
+            metadataMap.put("meta_username", "ygj6@cdc.gov");
+            metadataMap.put("meta_ext_filename", "10MB-test-file");
+            metadataMap.put("meta_ext_objectkey", UUID.randomUUID().toString());
+
+            TusClient client = new TusClient();
+            client.setUploadCreationURL(new URL(dotenv.get("DEX_URL")));
+            client.setHeaders(headerMap);
+
+            File file = new File("../../upload-files/10MB-test-file");
+            final TusUpload upload = new TusUpload(file);
+            upload.setMetadata(metadataMap);
+
+            System.out.println("Starting upload...");
+
+            TusExecutor executor = new TusExecutor() {
+                @Override
+                protected void makeAttempt() throws ProtocolException, IOException {
+                    TusUploader uploader = client.resumeOrCreateUpload(upload);
+                    uploader.setChunkSize(1024);
+
+                    do {
+                        long totalBytes = upload.getSize();
+                        long bytesUploaded = uploader.getOffset();
+                        double progress = (double) bytesUploaded / totalBytes * 100;
+
+                        System.out.printf("Upload at %06.2f%%.\n", progress);
+                    } while (uploader.uploadChunk() > -1);
+
+                    uploader.finish();
+
+                    System.out.println("Upload finished.");
+                    System.out.format("Upload available at: %s", uploader.getUploadURL().toString());
+                }
+            };
+
+            executor.makeAttempts();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
