15c44beef821194c64d5e8f590f32cf04ee4dc7d
cfarmer <cfarmer@fearless.tech>
2023-05-08 17:32:14 -0400

    cypress example

---
+++ tests/cypress/cypress.config.js
@@ -0,0 +1,13 @@
+const env = require("dotenv").config().parsed;
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+  env: {
+    ...env
+  }
+});
