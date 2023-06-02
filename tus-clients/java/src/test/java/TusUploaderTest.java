961b4d2e72ffd45b74e2098cbefa297497338f77
Linda Zhou <utf8@cdc.gov>
2023-06-02 14:27:47 -0500

    add unit test for tusUploader class

---
+++ tus-clients/java/src/test/java/TusUploaderTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TusUploaderTest {
+
+    @Test
+    public void testTusUploader() {
+        TusUploader tusUploader = new TusUploader();
+        uploader.TusUploader();
+    }
+
+}
