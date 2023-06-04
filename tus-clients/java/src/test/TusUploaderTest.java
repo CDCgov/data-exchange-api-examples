c8725f3da2999dd392ccd5ac3b08632eea5b1c02
Linda Zhou <utf8@cdc.gov>
2023-06-04 18:37:27 -0500

    add unit test to tusUploader

--- tus-clients/java/src/test/TusUploaderTest.java
+++ tus-clients/java/src/test/TusUploaderTest.java
@@ -1,29 +1,41 @@
 import io.github.cdimascio.dotenv.Dotenv;
+import io.tus.java.client.TusClient;
+import io.tus.java.client.TusExecutor;
+import io.tus.java.client.TusUploader;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
-import java.util.HashMap;
-import java.util.UUID;
+import java.net.URL;
 
 import static org.mockito.Mockito.*;
 
 class TusUploaderTest {
-    
+    private Dotenv dotenv;
+    private TusClient client;
+    private TusUploader uploader;
+
+    @BeforeEach
+    void setUp() {
+        dotenv = mock(Dotenv.class);
+        client = mock(TusClient.class);
+        uploader = new TusUploader(dotenv, client);
+    }
+
     @Test
-    void tusUploaderTest() throws Exception {
-        // Mock the dependencies
-        Dotenv dotenv = mock(Dotenv.class);
-        TusClient client = mock(TusClient.class);
-        
+    void makeUploadTest() throws Exception {
         // Set up the mock behavior
         when(dotenv.get("AUTH_TOKEN")).thenReturn("dummy_token");
-        when(dotenv.get(anyString())).thenReturn("dummy_value");
-        when(client.upload(Mockito.any(), Mockito.anyBoolean())).thenReturn(mock(TusUploader.class));
-        
-        TusUploader uploader = new TusUploader(dotenv, client);
-        uploader.TusUploader(); // Call the method
-        
-        // Add assertions to verify the behavior and results
-        // ...
+        when(dotenv.get("DEX_URL")).thenReturn("https://example.com");
+
+        TusExecutor executor = mock(TusExecutor.class);
+        when(client.resumeOrCreateUpload(any())).thenReturn(uploader);
+        when(uploader.getOffset()).thenReturn(0L, 512L, 1024L);
+        when(uploader.uploadChunk()).thenReturn(512, 512, -1);
+        when(uploader.getUploadURL()).thenReturn(new URL("https://example.com/upload"));
+
+        whenNew(TusExecutor.class).withNoArguments().thenReturn(executor);
+
+        uploader.makeUpload();
     }
 }
