bca59007147f849ef0811b220518db9097af6d36
cfarmer <cfarmer@fearless.tech>
2023-06-05 16:38:10 -0400

    tests for destination status

--- tests/cypress/cypress/e2e/status.spec.cy.js
+++ tests/cypress/cypress/e2e/status.spec.cy.js
@@ -56,4 +56,62 @@ describe("status", () => {
         })
     })
   })
+
+  describe("/status/destination/{destinationName}", () => {
+    it("should return unauthorized when no auth token provided", () => {
+      cy.api({
+        url: `${Cypress.env("DEX_URL")}/status/destination/blah`,
+        method: "GET",
+        failOnStatusCode: false
+      })
+        .then(resp => {
+          expect(resp.status).to.eq(401)
+        })
+    })
+
+    it("should return unauthorized when invalid auth token provided", () => {
+      cy.api({
+        url: `${Cypress.env("DEX_URL")}/status/destination/blah`,
+        method: "GET",
+        headers: {
+          "Authorization": "Bearer 12345"
+        },
+        failOnStatusCode: false
+      })
+        .then(resp => {
+          expect(resp.status).to.eq(401)
+        })
+    })
+
+    it("should return empty page when non-existent destination name provided", async () => {
+      const accessToken = await new Promise((res, rej) => {
+        cy.api({
+          url: `${Cypress.env("DEX_URL")}/oauth`,
+          method: "POST",
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded"
+          },
+          body: {
+            username: Cypress.env("SAMS_USERNAME"),
+            password: Cypress.env("SAMS_PASSWORD")
+          }
+        })
+          .then(resp => res(resp.body["access_token"]))
+      })
+  
+      expect(accessToken).to.not.be.undefined;
+
+      cy.api({
+        url: `${Cypress.env("DEX_URL")}/status/destination/does_not_exist`,
+        method: "GET",
+        headers: {
+          "Authorization": `Bearer ${accessToken}`
+        }
+      })
+        .then(resp => {
+          expect(resp.status).to.eq(200)
+          expect(resp.body["summary"]["total_items"]).to.eq(0)
+        })
+    })
+  })
 })
